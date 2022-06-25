e73dff3e32e4f7a6cdc422d5ecc58940afcf0ec4
Michael Kamprath <michael@kamprath.net>
2022-06-25 01:37:03 -0700

    added minhex pretty printer

---
+++ bespokeasm/assembler/pretty_printer/minhex.py
@@ -0,0 +1,37 @@
+import io
+import math
+
+from bespokeasm.assembler.line_object import LineWithBytes, LineObject
+from bespokeasm.assembler.model import AssemblerModel
+from bespokeasm.assembler.pretty_printer import PrettyPrinterBase
+from bespokeasm.assembler.line_object.directive_line import AddressOrgLine
+
+class MinHexPrettyPrinter(PrettyPrinterBase):
+    def __init__(self, line_objs:  list[LineObject], model: AssemblerModel) -> None:
+        super().__init__(line_objs, model)
+
+    def pretty_print(self, max_instruction_text_size: int) -> str:
+        output = io.StringIO()
+        line_byte_count = 0
+        address_width = int(self.model.address_size/4)
+        for lobj in self.line_objects:
+            if isinstance(lobj, LineWithBytes):
+                line_bytes = lobj.get_bytes()
+                for b in line_bytes:
+                    if line_byte_count == 0:
+                        output.write(':')
+                    output.write(f'{b:02x} ')
+                    line_byte_count += 1
+                    if line_byte_count == 16:
+                        output.write('\n')
+                        line_byte_count = 0
+            elif isinstance(lobj, AddressOrgLine):
+                if line_byte_count != 0:
+                    output.write('\n')
+                    line_byte_count = 0
+                output.write('{addr:0{width}x}\n'.format(addr=lobj.address, width=address_width))
+
+        if line_byte_count != 0:
+            output.write('\n')
+
+        return output.getvalue()
