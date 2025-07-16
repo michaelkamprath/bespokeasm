7dae31a0a167eb419e1dcbac9552e56062703918
Michael Kamprath <michael@kamprath.net>
2025-07-15 17:53:56 -0700

    added 16-bit word engine test

--- test/test_engine.py
+++ test/test_engine.py
@@ -109,3 +109,111 @@ class TestAssemblerEngine(unittest.TestCase):
             bytearray([0, 0x88, 0b01000110, 1, 0]),
             'the bytecode should match',
         )
+
+    def test_generate_bytes_from_line_objects_16bit_words(self):
+        fp = pkg_resources.files(config_files).joinpath('test_16bit_data_words.yaml')
+        isa_model = AssemblerModel(str(fp), 0)
+        memzone_mngr = MemoryZoneManager(
+            isa_model.address_size,
+            isa_model.default_origin,
+            isa_model.predefined_memory_zones,
+        )
+
+        label_values = GlobalLabelScope(isa_model.registers)
+        label_values.set_label_value('a_const', 40, 1)
+        preprocessor = Preprocessor()
+        condition_stack = ConditionStack()
+        line_objects: list[LineObject] = []
+
+        line_objects.extend(
+            LineOjectFactory.parse_line(
+                LineIdentifier(0, 'test_generate_bytes_from_line_objects_16bit_words'),
+                'nop ; do nothing',
+                isa_model,
+                label_values,
+                memzone_mngr.global_zone,
+                memzone_mngr,
+                preprocessor,
+                condition_stack,
+                0,
+            )
+        )
+
+        line_objects.extend(
+            LineOjectFactory.parse_line(
+                LineIdentifier(0, 'test_generate_bytes_from_line_objects_16bit_words'),
+                'mov [$1234], [$8899] ; move it',
+                isa_model,
+                label_values,
+                memzone_mngr.global_zone,
+                memzone_mngr,
+                preprocessor,
+                condition_stack,
+                0,
+            )
+        )
+
+        line_objects.extend(
+            LineOjectFactory.parse_line(
+                LineIdentifier(0, 'test_generate_bytes_from_line_objects_16bit_words'),
+                'my_label: push [$1234] ; push it real good',
+                isa_model,
+                label_values,
+                memzone_mngr.global_zone,
+                memzone_mngr,
+                preprocessor,
+                condition_stack,
+                0,
+            )
+        )
+
+        line_objects.extend(
+            LineOjectFactory.parse_line(
+                LineIdentifier(0, 'test_generate_bytes_from_line_objects_16bit_words'),
+                'jmp my_label ; get out of here',
+                isa_model,
+                label_values,
+                memzone_mngr.global_zone,
+                memzone_mngr,
+                preprocessor,
+                condition_stack,
+                0,
+            )
+        )
+
+        # "assemble" the line objects to a dictionary
+        line_dict: dict[int, LineObject] = {}
+        for lobj in line_objects:
+            lobj.set_start_address(lobj.memory_zone.current_address)
+            lobj.memory_zone.current_address = lobj.address + lobj.word_count
+            lobj.label_scope = label_values
+            if isinstance(lobj, LabelLine) and not lobj.is_constant:
+                lobj.label_scope.set_label_value(lobj.get_label(), lobj.get_value(), lobj.line_id)
+            line_dict[lobj.address] = lobj
+
+        line_objects.sort(key=lambda x: x.address)
+        max_generated_address = line_objects[-1].address
+        for lobj in line_objects:
+            if isinstance(lobj, LineWithWords):
+                lobj.generate_words()
+        # generate the bytecode
+        bytecode = Assembler._generate_bytes(
+            line_dict,
+            max_generated_address,
+            bytearray([0x00]),
+            memzone_mngr.global_zone.start,
+            None,
+            2,
+        )
+
+        # now see if the right bytes were generated
+        self.assertEqual(
+            bytecode,
+            bytearray([
+                0, 0,  # address 0
+                8, 0x22, 0x88, 0x99, 0x12, 0x34,  # address 1, 2, 3
+                0x04, 0x02, 0x12, 0x34,  # address 4
+                0x80, 0x01, 0x00, 0x04,  # address 5
+            ]),
+            'the bytecode should match',
+        )
