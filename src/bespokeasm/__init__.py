21b42a3d142c74f1ee225a92e90c44d88c60a9bc
Michael Kamprath <michael@kamprath.net>
2025-09-10 09:25:36 -0700

    VIM syntax highlighting (#43)
    Added a new vim option to the generate-extension command. This will create a VIM syntax highlighting configuration file for the passed instruction set.

--- src/bespokeasm/__init__.py
+++ src/bespokeasm/__init__.py
@@ -1,4 +1,4 @@
-BESPOKEASM_VERSION_STR = '0.5.2b2'
+BESPOKEASM_VERSION_STR = '0.5.2b3'
 
 # if a cconfig file requires a certain bespoke ASM version, it should be at least this version.
 BESPOKEASM_MIN_REQUIRED_STR = '0.5.0'
